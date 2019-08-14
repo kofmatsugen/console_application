3a6e679b9c90d22183d41c284ae66642daf50e91
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2019-08-14 21:02:04 +0900

    log を logger に移行

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use sprite_studio_converter::convert_to_file;
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    env_logger::init()?;
+    println!(
+        "{:?}",
+        convert_to_file(
+            "data/sprite_studio/converted",
+            "data/sprite_studio/houou.sspj",
+        )?
+    );
+
+    Ok(())
+}
