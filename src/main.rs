1eb50370c5a910b89051d22076f943b0fbf0a915
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2019-08-25 17:45:20 +0900

    timeline 移行に対応

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use sprite_studio_converter::prefab::convert_to_file;
+use sprite_studio_converter::convert_to_timeline;
 
 fn main() -> Result<(), Box<std::error::Error>> {
     env_logger::init();
     println!(
         "{:?}",
-        convert_to_file(
+        convert_to_timeline(
             "data/sprite_studio/converted",
             "data/sprite_studio/houou.sspj",
         )?
