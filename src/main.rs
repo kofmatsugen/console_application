07dafad92fa6dec321005ac669e0b02093a6ef52
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2019-09-16 10:43:39 +0900

    今後引数でやりたいけど，一旦全部コンバート

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use sprite_studio_converter::convert_to_timeline;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    println!(
-        "{:?}",
-        convert_to_timeline(
-            "amethyst_sandbox/resources",
-            "data/sprite_studio/studio_logo/splash1024.sspj",
-        )?
-    );
+    convert_to_timeline(
+        "amethyst_sandbox/resources",
+        "data/sprite_studio/studio_logo/splash1024.sspj",
+    )?;
+    convert_to_timeline(
+        "amethyst_sandbox/resources",
+        "data/sprite_studio/houou/houou.sspj",
+    )?;
 
     Ok(())
 }
