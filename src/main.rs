305224682e9c5e353f5a4098403479fa9291474c
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2019-10-02 22:50:07 +0900

    格ゲー用のデータに変換するように変更

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
+use fight_game::paramater::AnimationParam;
 use sprite_studio_converter::convert_to_timeline;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    convert_to_timeline(
+    convert_to_timeline::<_, AnimationParam>(
         "amethyst_sandbox/resources",
         "data/sprite_studio/studio_logo/splash1024.sspj",
     )?;
-    convert_to_timeline(
+    convert_to_timeline::<_, AnimationParam>(
         "amethyst_sandbox/resources",
         "data/sprite_studio/houou/houou.sspj",
     )?;
-    convert_to_timeline(
+    convert_to_timeline::<_, AnimationParam>(
         "amethyst_sandbox/resources",
         "data/sprite_studio/template/character_template1.sspj",
     )?;
-    convert_to_timeline(
+    convert_to_timeline::<_, AnimationParam>(
         "amethyst_sandbox/resources",
         "data/sprite_studio/sample/sample.sspj",
     )?;
