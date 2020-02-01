d74dfc4fa384199aa17669d8b1c3b2575a3cafcf
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2020-02-01 21:37:38 +0900

    データ変換をID値を使って行うように変更

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-use fight_game::paramater::AnimationParam;
+use fight_game::{
+    id::pack::{AnimationKey, PackKey},
+    paramater::AnimationParam,
+};
 use sprite_studio_converter::convert_to_timeline;
 
 fn main() -> Result<(), failure::Error> {
     env_logger::init();
 
-    convert_to_timeline::<_, AnimationParam>(
-        "amethyst_sandbox/resources",
-        "data/sprite_studio/studio_logo/splash1024.sspj",
-    )?;
-    convert_to_timeline::<_, AnimationParam>(
-        "amethyst_sandbox/resources",
-        "data/sprite_studio/houou/houou.sspj",
-    )?;
-    convert_to_timeline::<_, AnimationParam>(
-        "amethyst_sandbox/resources",
-        "data/sprite_studio/template/character_template1.sspj",
-    )?;
-    convert_to_timeline::<_, AnimationParam>(
+    // convert_to_timeline::<_, AnimationParam, PackKey, AnimationKey>(
+    //     "amethyst_sandbox/resources",
+    //     "data/sprite_studio/studio_logo/splash1024.sspj",
+    // )?;
+    // convert_to_timeline::<_, AnimationParam, PackKey, AnimationKey>(
+    //     "amethyst_sandbox/resources",
+    //     "data/sprite_studio/houou/houou.sspj",
+    // )?;
+    // convert_to_timeline::<_, AnimationParam, PackKey, AnimationKey>(
+    //     "amethyst_sandbox/resources",
+    //     "data/sprite_studio/template/character_template1.sspj",
+    // )?;
+    convert_to_timeline::<_, AnimationParam, PackKey, AnimationKey>(
         "amethyst_sandbox/resources",
         "data/sprite_studio/sample/sample.sspj",
     )?;
